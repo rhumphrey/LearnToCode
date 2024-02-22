6b52e38612f2b0b4da760902fd6a482c91294a00
rhumphrey <robinhumphrey@hotmail.com>
2024-02-22 09:05:36 -0400

    Add files via upload

---
+++ 01_Ruby/experiments/day3.rb
@@ -0,0 +1,3 @@
+# Day 3 experiments
+
+# Open Classes
---
+++ 01_Ruby/experiments/day3hw.rb
@@ -0,0 +1,4 @@
+# Day 3 Self-Study (Homework)
+# Do:
+#   1. Modify the CSV application to support an each method to return a CsvRow object. 
+#      Use method_missing on that CsvRow to return the value for the column for a given heading
